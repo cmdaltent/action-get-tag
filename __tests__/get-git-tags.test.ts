26420f9107aa0aa2e4e19f40c6eaa914b62ee001
Martin Weissbach <martin@cmdaltent.net>
2023-12-17 10:14:40 +0100

    test: mock exec call to repository
    GTA-9
    
    Signed-off-by: Martin Weissbach <martin@cmdaltent.net>

--- __tests__/get-git-tags.test.ts
+++ __tests__/get-git-tags.test.ts
@@ -1,5 +1,29 @@
 import {describe, it, expect} from '@jest/globals'
 import getLatestGitTag, {getGitTags} from '../src/get-git-tags'
+import {jest} from '@jest/globals'
+import {exec} from 'child_process'
+
+type ExecCallback = Parameters<typeof exec>[2]
+
+interface ChildProcess {
+  exec: (command: string, cb?: ExecCallback) => void
+}
+
+const refs = `
+refs/tags/v1.0.0 Fri Sep 1 16:31:45 2023 +0200
+refs/tags/v1.0.1 Fri Sep 29 19:13:49 2023 +0200
+refs/tags/v1 Mon Oct 2 09:35:58 2023 +0200
+`
+
+jest.mock('child_process', () => {
+  const cp = jest.createMockFromModule<ChildProcess>('child_process')
+  cp.exec = jest.fn((_command, cb) => {
+    if (cb) {
+      cb(null, refs, '')
+    }
+  })
+  return cp
+})
 
 describe('get-git-tags', () => {
   it('should return a list of tags', async () => {
@@ -7,7 +31,7 @@ describe('get-git-tags', () => {
     const actual = await getGitTags()
     expect(actual).toStrictEqual(expected)
   })
-  it('should get the latest tag', async () => {
+  it('should get the newest tag', async () => {
     const expected = 'v1.0.1'
     const actual = await getLatestGitTag()
     expect(actual).toBe(expected)
@@ -17,7 +41,7 @@ describe('get-git-tags', () => {
     const actual = await getGitTags(/v\d*$/)
     expect(actual).toStrictEqual(expected)
   })
-  it('should return only the latest tag that matches the given expression', async () => {
+  it('should return only the newest tag that matches the given expression', async () => {
     const expected = 'v1'
     const actual = await getLatestGitTag(/v\d*$/)
     expect(actual).toBe(expected)
